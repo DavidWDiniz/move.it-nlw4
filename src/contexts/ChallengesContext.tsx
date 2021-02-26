18e2ba63f39f22b21ac5bf38b28efb1a6158c723
DavidWDiniz <davidwdiniz@gmail.com>
2021-02-26 09:52:59 -0300

    Fourth day - Next Level Week #04

--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -1,4 +1,4 @@
-import {createContext, ReactNode, useState} from 'react';
+import {createContext, ReactNode, useEffect, useState} from 'react';
 import challenges from '../../challenges.json';
 
 type Challenge = {
@@ -16,6 +16,7 @@ type ChallengesContextData = {
     levelUp: () => void;
     startNewChallenge: () => void;
     resetChallenge: () => void;
+    completeChallenge: () => void;
 }
 
 type ChallengesProviderProps = {
@@ -31,7 +32,11 @@ export function ChallengesProvider({children}: ChallengesProviderProps) {
 
     const [activeChallenge, setActiveChallenge] = useState(null);
 
-    const experienceToNextLevel = Math.pow((level + 1) * 4, 2)
+    const experienceToNextLevel = Math.pow((level + 1) * 4, 2);
+
+    useEffect(() => {
+        Notification.requestPermission();
+    }, []);
 
     function levelUp() {
         setLevel(level + 1);
@@ -41,10 +46,34 @@ export function ChallengesProvider({children}: ChallengesProviderProps) {
         const randomChallengeIndex = Math.floor(Math.random() * challenges.length);
         const challenge = challenges[randomChallengeIndex];
         setActiveChallenge(challenge);
+
+        new Audio('/notification.mp3').play();
+
+        if (Notification.permission === 'granted') {
+            new Notification('Novo desafio', {body: `Valendo ${challenge.amount} xp!`})
+        }
     }
 
     function resetChallenge() {
         setActiveChallenge(null);
+    }
+
+    function completeChallenge() {
+        if (!activeChallenge) {
+            return;
+        }
+
+        const {amount} = activeChallenge;
+        let finalExperience = currentExperience + amount;
+
+        if (finalExperience >= experienceToNextLevel) {
+            finalExperience = finalExperience - experienceToNextLevel;
+            levelUp();
+        }
+
+        setCurrentExperience(finalExperience);
+        setActiveChallenge(null);
+        setChallengesCompleted(challengesCompleted + 1);
     }
 
     return (
@@ -57,7 +86,8 @@ export function ChallengesProvider({children}: ChallengesProviderProps) {
                 startNewChallenge,
                 activeChallenge,
                 resetChallenge,
-                experienceToNextLevel
+                experienceToNextLevel,
+                completeChallenge,
             }}>
             {children}
         </ChallengesContext.Provider>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,11 @@
-import Head from "next/head";
+import Head from 'next/head';
 
 import {ExperienceBar} from '../components/ExperienceBar';
 import {Profile} from '../components/Profile';
 import {CompletedChallenges} from '../components/CompletedChallenges';
 import {Countdown} from '../components/Countdown';
 import {ChallengeBox} from "../components/ChallengeBox";
+import {CountdownProvider} from '../contexts/CountdownContext';
 
 import styles from '../styles/pages/Home.module.css';
 
@@ -15,16 +16,18 @@ export default function Home() {
                 <title>Início | move.it</title>
             </Head>
             <ExperienceBar/>
-            <section>
-                <div>
-                    <Profile/>
-                    <CompletedChallenges/>
-                    <Countdown/>
-                </div>
-                <div>
-                    <ChallengeBox/>
-                </div>
-            </section>
+            <CountdownProvider>
+                <section>
+                    <div>
+                        <Profile/>
+                        <CompletedChallenges/>
+                        <Countdown/>
+                    </div>
+                    <div>
+                        <ChallengeBox/>
+                    </div>
+                </section>
+            </CountdownProvider>
         </div>
-    )
+    );
 }
